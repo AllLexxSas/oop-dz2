9b82a21d6afb49ac419286138074e2b33bf211d2
AllLexxSas <wqqe82@gmail.com>
2021-09-27 17:49:48 +0300

    Домашнее задание.

---
+++ black_jack_game.h
@@ -0,0 +1,26 @@
+#ifndef BLACK_JACK_GAME_H
+#define BLACK_JACK_GAME_H
+#include <iostream>
+
+class Card
+{
+private:
+    std::string cardName;
+};
+
+class ColorСard : Card
+{
+
+};
+
+class DesignationCard: ColorСard
+{
+
+};
+
+class Player
+{
+
+};
+
+#endif // BLACK_JACK_GAME_H
